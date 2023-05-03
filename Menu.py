231daad8365bfa0ef99962dc27300d8be1c4e654
Holocron <108415700+Ho1ocron@users.noreply.github.com>
2023-05-03 22:44:33 +0500

    Add files via upload

--- Menu.py
+++ Menu.py
@@ -1,13 +1,19 @@
 import os
 import sys
-import New_game
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PIL import Image
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
 from PyQt5.QtGui import QPalette, QBrush, QPixmap
 from PyQt5.QtGui import QPalette
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import pyqtSlot
 import threading
 
+
+def image_resize():
+    image = Image.open('bg1.png')
+    new_image = image.resize((190, 108))
+    new_image.save('bg1.png')
+    return new_image
 
 class App(QWidget):
 
@@ -19,6 +25,7 @@ class App(QWidget):
         self.width = 800
         self.height = 600
         self.initUI()
+        image_resize()
 
     def initUI(self):
         
@@ -270,123 +277,129 @@ class App(QWidget):
 
         """Background shop"""
 
-        #: First background
-        self.bg0 = QPushButton('Dark ground', self)
-        self.bg0.setFont(QFont('Arial', 35))
-        self.bg0.setStyleSheet("background-color: rgba(255, 255, 255, 75);\n""border: rgba(255, 255, 255, 100);") 
-        self.bg0.resize(400, 55) 
-        self.bg0.move(200, 175)
-        #self.bg0.clicked.connect(self.user_map_exit) 
-        self.bg0.hide() 
+        #: BG choose
+        self.BGNEXT = QPushButton('>>', self)
+        self.BGNEXT.setFont(QFont('Arial', 32))
+        self.BGNEXT.setStyleSheet("background-color: rgba(255, 255, 255, 75);\n""border: rgba(255, 255, 255, 100);") 
+        self.BGNEXT.resize(100, 44) 
+        self.BGNEXT.move(700, 278)
+        self.BGNEXT.hide()
 
-        #: Second background
-        self.bg1 = QPushButton('Light background', self)
-        self.bg1.setFont(QFont('Arial', 35))
-        self.bg1.setStyleSheet("background-color: rgba(255, 255, 255, 75);\n""border: rgba(255, 255, 255, 100);") 
-        self.bg1.resize(400, 55) 
-        self.bg1.move(200, 240)
-        #self.bg1.clicked.connect(self.car_buy) 
-        self.bg1.hide()
+        self.BGPREV = self.NEXT = QPushButton('<<', self)
+        self.BGPREV.setFont(QFont('Arial', 32))
+        self.BGPREV.setStyleSheet("background-color: rgba(255, 255, 255, 75);\n""border: rgba(255, 255, 255, 100);") 
+        self.BGPREV.resize(100, 44) 
+        self.BGPREV.move(0, 278)
+        self.BGPREV.hide()
 
-        #: Third background
-        self.bg2 = QPushButton('Cust background', self)
-        self.bg2.setFont(QFont('Arial', 35))
-        self.bg2.setStyleSheet("background-color: rgba(255, 255, 255, 75);\n""border: rgba(255, 255, 255, 100);") 
-        self.bg2.resize(400, 55) 
-        self.bg2.move(200, 305)
-        #self.bg2.clicked.connect(self.map_buy) 
-        self.bg2.hide()
+        self.bg_ch_btn = QPushButton('Simple BG', self)
+        self.bg_ch_btn.setFont(QFont('Arial', 35))
+        self.bg_ch_btn.setStyleSheet("background-color: rgba(255, 255, 255, 75);\n""border: rgba(255, 255, 255, 100);") 
+        self.bg_ch_btn.resize(400, 55) 
+        self.bg_ch_btn.move(200, 370)
+        self.bg_ch_btn.hide()
 
         #: exit from background shop
         self.bg_exit = QPushButton('Back', self)
         self.bg_exit.setFont(QFont('Arial', 35))
         self.bg_exit.setStyleSheet("background-color: rgba(255, 255, 255, 75);\n""border: rgba(255, 255, 255, 100);") 
         self.bg_exit.resize(400, 55) 
-        self.bg_exit.move(200, 370)
+        self.bg_exit.move(200, 435)
         self.bg_exit.clicked.connect(self.bg_exit_shop) 
         self.bg_exit.hide()
 
-        self.bg_list = [self.bg0, self.bg1, self.bg2, self.bg_exit]
+        self.bg_shop_cmd_list = [self.BGNEXT, self.BGPREV, self.bg_exit, self.bg_ch_btn]
+
 
         """Car shop"""
 
-        #: 1st car
-        self.car0 = QPushButton('Dark car', self)
-        self.car0.setFont(QFont('Arial', 35))
-        self.car0.setStyleSheet("background-color: rgba(255, 255, 255, 75);\n""border: rgba(255, 255, 255, 100);") 
-        self.car0.resize(400, 55) 
-        self.car0.move(200, 175)
-        #self.car0.clicked.connect(self.user_map_exit) 
-        self.car0.hide() 
+        #: Cars
+        self.car1img = QLabel(self)
+        self.car1imgpix = QPixmap("car1.png")
+        self.car1img.setPixmap(self.car1imgpix)
+        self.car1img.move(305, 246)
+        self.car1img.hide()
 
-        #: 2nd car
-        self.car1 = QPushButton('Light car', self)
-        self.car1.setFont(QFont('Arial', 35))
-        self.car1.setStyleSheet("background-color: rgba(255, 255, 255, 75);\n""border: rgba(255, 255, 255, 100);") 
-        self.car1.resize(400, 55) 
-        self.car1.move(200, 240)
-        #self.car1.clicked.connect(self.car_buy) 
-        self.car1.hide()
+        self.car2img = QLabel(self)
+        self.car2imgpix = QPixmap("bg1.png")
+        self.car2img.setPixmap(self.car2imgpix)
+        self.car2img.move(305, 246)
+        self.car2img.hide()
 
-        #: 3rd car
-        self.car2 = QPushButton('Cust car', self)
-        self.car2.setFont(QFont('Arial', 35))
-        self.car2.setStyleSheet("background-color: rgba(255, 255, 255, 75);\n""border: rgba(255, 255, 255, 100);") 
-        self.car2.resize(400, 55) 
-        self.car2.move(200, 305)
-        #self.car2.clicked.connect(self.map_buy) 
-        self.car2.hide()
+        self.car_list = [self.car1img, self.car2img]
+        self.car_n = 0
+
+        #: Car choose
+        self.CARNEXT = QPushButton('>>', self)
+        self.CARNEXT.setFont(QFont('Arial', 32))
+        self.CARNEXT.setStyleSheet("background-color: rgba(255, 255, 255, 75);\n""border: rgba(255, 255, 255, 100);") 
+        self.CARNEXT.resize(100, 44) 
+        self.CARNEXT.move(700, 278)
+        self.CARNEXT.clicked.connect(self.car_next_fn) 
+        self.CARNEXT.hide()
+
+        self.CARPREV = self.NEXT = QPushButton('<<', self)
+        self.CARPREV.setFont(QFont('Arial', 32))
+        self.CARPREV.setStyleSheet("background-color: rgba(255, 255, 255, 75);\n""border: rgba(255, 255, 255, 100);") 
+        self.CARPREV.resize(100, 44) 
+        self.CARPREV.move(0, 278)
+        self.CARPREV.clicked.connect(self.car_next_fn) 
+        self.CARPREV.hide()
+
+        self.car_ch_btn = QPushButton('Simple car', self)
+        self.car_ch_btn.setFont(QFont('Arial', 35))
+        self.car_ch_btn.setStyleSheet("background-color: rgba(255, 255, 255, 75);\n""border: rgba(255, 255, 255, 100);") 
+        self.car_ch_btn.resize(400, 55) 
+        self.car_ch_btn.move(200, 370)
+        self.car_ch_btn.hide()
 
         #: exit from car shop
         self.car_exit = QPushButton('Back', self)
         self.car_exit.setFont(QFont('Arial', 35))
         self.car_exit.setStyleSheet("background-color: rgba(255, 255, 255, 75);\n""border: rgba(255, 255, 255, 100);") 
         self.car_exit.resize(400, 55) 
-        self.car_exit.move(200, 370)
+        self.car_exit.move(200, 435)
         self.car_exit.clicked.connect(self.car_exit_shop) 
         self.car_exit.hide()
 
-        self.car_list = [self.car0, self.car1, self.car2, self.car_exit]
+        self.car_shop_cmd_list = [self.CARNEXT, self.CARPREV, self.car_exit, self.car_ch_btn]
+
+        #self.car_list = [self.car0, self.car1, self.car2, self.car_exit]
 
         """Map shop"""
         
-        #: 1st map
-        self.map0 = QPushButton('Dark map', self)
-        self.map0.setFont(QFont('Arial', 35))
-        self.map0.setStyleSheet("background-color: rgba(255, 255, 255, 75);\n""border: rgba(255, 255, 255, 100);") 
-        self.map0.resize(400, 55) 
-        self.map0.move(200, 175)
-        #self.map0.clicked.connect(self.user_map_exit) 
-        self.map0.hide() 
+        #: Map choose
+        self.MAPNEXT = QPushButton('>>', self)
+        self.MAPNEXT.setFont(QFont('Arial', 32))
+        self.MAPNEXT.setStyleSheet("background-color: rgba(255, 255, 255, 75);\n""border: rgba(255, 255, 255, 100);") 
+        self.MAPNEXT.resize(100, 44) 
+        self.MAPNEXT.move(700, 278)
+        self.MAPNEXT.hide()
 
-        #: 2nd map
-        self.map1 = QPushButton('Light map', self)
-        self.map1.setFont(QFont('Arial', 35))
-        self.map1.setStyleSheet("background-color: rgba(255, 255, 255, 75);\n""border: rgba(255, 255, 255, 100);") 
-        self.map1.resize(400, 55) 
-        self.map1.move(200, 240)
-        #self.map1.clicked.connect(self.car_buy) 
-        self.map1.hide()
+        self.MAPPREV = QPushButton('<<', self)
+        self.MAPPREV.setFont(QFont('Arial', 32))
+        self.MAPPREV.setStyleSheet("background-color: rgba(255, 255, 255, 75);\n""border: rgba(255, 255, 255, 100);") 
+        self.MAPPREV.resize(100, 44) 
+        self.MAPPREV.move(0, 278)
+        self.MAPPREV.hide()
 
-        #: 3rd map
-        self.map2 = QPushButton('Cust map', self)
-        self.map2.setFont(QFont('Arial', 35))
-        self.map2.setStyleSheet("background-color: rgba(255, 255, 255, 75);\n""border: rgba(255, 255, 255, 100);") 
-        self.map2.resize(400, 55) 
-        self.map2.move(200, 305)
-        #self.map2.clicked.connect(self.map_buy) 
-        self.map2.hide()
+        self.map_ch_btn = QPushButton('Simple map', self)
+        self.map_ch_btn.setFont(QFont('Arial', 35))
+        self.map_ch_btn.setStyleSheet("background-color: rgba(255, 255, 255, 75);\n""border: rgba(255, 255, 255, 100);") 
+        self.map_ch_btn.resize(400, 55) 
+        self.map_ch_btn.move(200, 370)
+        self.map_ch_btn.hide()
 
         #: exit from map shop
         self.map_exit = QPushButton('Back', self)
         self.map_exit.setFont(QFont('Arial', 35))
         self.map_exit.setStyleSheet("background-color: rgba(255, 255, 255, 75);\n""border: rgba(255, 255, 255, 100);") 
         self.map_exit.resize(400, 55) 
-        self.map_exit.move(200, 370)
+        self.map_exit.move(200, 435)
         self.map_exit.clicked.connect(self.map_exit_shop) 
         self.map_exit.hide()
-
-        self.map_list = [self.map0, self.map1, self.map2, self.map_exit]
+        
+        self.map_shop_cmd_list = [self.MAPNEXT, self.MAPPREV, self.map_exit, self.map_ch_btn]
 
         self.show()
     
@@ -420,24 +433,48 @@ class App(QWidget):
     def bg_buy(self):
         for i in self.shop_list:
             i.hide()
-        
-        for j in self.bg_list:
+
+        for j in self.bg_shop_cmd_list:
             j.show()
 
     def car_buy(self):
         for i in self.shop_list:
             i.hide()
-        
-        for j in self.car_list:
+
+        for j in self.car_shop_cmd_list:
             j.show()
+        self.car1img.show()
     
+    def car_next_fn(self):
+        self.car_list[self.car_n].show()
+        if self.car_n != len(self.car_list)-1:
+            self.car_n += 1
+            self.car_list[self.car_n].show()
+            self.car_list[self.car_n-1].hide()
+        else:
+            self.car_n = 0
+            self.car_list[self.car_n].show()
+            self.car_list[self.car_n-1].hide()
+    
+    def car_prev_fn(self):
+        self.car_list[self.car_n].show()
+        if self.car_n != 0:
+            self.car_n -= 1
+            self.car_list[self.car_n].show()
+            self.car_list[self.car_n+1].hide()
+        else:
+            self.car_n = 1
+            self.car_list[self.car_n].show()
+            self.car_list[self.car_n+1].hide()
+
+
     def map_buy(self):
         for i in self.shop_list:
             i.hide()
         
-        for j in self.map_list:
+        for j in self.map_shop_cmd_list:
             j.show()
-
+        
     def create_map(self):
         pass
 
@@ -514,7 +551,7 @@ class App(QWidget):
         
     
     def bg_exit_shop(self):
-        for i in self.bg_list:
+        for i in self.bg_shop_cmd_list:
             i.hide()
         
         for j in self.shop_list:
@@ -522,14 +559,16 @@ class App(QWidget):
     
     
     def car_exit_shop(self):
-        for i in self.car_list:
+        for i in self.car_shop_cmd_list:
             i.hide()
+        self.car1img.hide()
         
         for j in self.shop_list:
             j.show()
+        
     
     def map_exit_shop(self):
-        for i in self.map_list:
+        for i in self.map_shop_cmd_list:
             i.hide()
         
         for j in self.shop_list:
