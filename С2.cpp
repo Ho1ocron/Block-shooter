9f8fc3d480af1850591344348e803cf160b4d1ee
Holocron <108415700+Ho1ocron@users.noreply.github.com>
2023-05-27 17:34:07 +0500

    Add files via upload

---
+++ С2.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+
+int input()
+{
+	cout << "X: ";
+	int x;
+	cin >> x;
+	return x;
+}
+
+
+void loop()
+{
+	int x = input();
+	for (int i = 0; i < 100; i++) {
+		if (i == 10) {
+			cout << "I is: " << i << endl;
+		}
+
+		else if (i == 20) {
+			cout << "I is: " << i << endl;
+		}
+
+		else if (i == x) {
+			cout << "I " << "(" << i << ") " << "is equal to X " << "(" << x << ")" << endl;
+		}
+	}
+}
+
+
+void str()
+{
+	cout << "Enter your string: ";
+	string s;
+	cin >> s;
+	cout << "Your string is: " << s << endl;
+}
+
+void str1()
+{
+	string nazv_sh;
+	cout << ": ";
+	getline(cin, nazv_sh);
+}
+
+void main() 
+{
+	loop();
+	str();
+	//str1();
+}
